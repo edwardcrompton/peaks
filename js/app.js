12692d0d62e801fc3da7cb2f92253d0a0b08e05c
Edward Crompton <edward.crompton@gmail.com>
2017-06-22 20:31:42 +0100

    Converting the using the geodesy library and removing redundant code

--- js/app.js
+++ js/app.js
@@ -13,45 +13,6 @@ $(document).ready(() => {
     return !isNaN(parseFloat(n)) && isFinite(n);
   }
 
-  function squared(x) {
-    return x * x;
-  }
-  function toRad(x) {
-    return (x * PI) / 180.0;
-  }
-  function toDegree(x) {
-    return (x * 180) / PI;
-  }
-
-  // https://stackoverflow.com/questions/1502590/calculate-distance-between-two-points-in-google-maps-v3
-  function haversineDistance(a, b) {
-    const aLat = a.latitude || a.lat;
-    const bLat = b.latitude || b.lat;
-    const aLng = a.longitude || a.lng;
-    const bLng = b.longitude || b.lng;
-
-    const dLat = toRad(bLat - aLat);
-    const dLon = toRad(bLng - aLng);
-
-    const f = squared(sin(dLat / 2.0)) + (
-      cos(toRad(aLat)) * cos(toRad(bLat)) * squared(sin(dLon / 2.0))
-    );
-    const c = 2 * atan2(sqrt(f), sqrt(1 - f));
-
-    return Math.round((R * c) / 1000);
-  }
-
-  // Bearing calculation from https://stackoverflow.com/questions/11415106/issue-with-calcuating-compass-bearing-between-two-gps-coordinates#11415329
-  function bearing(lat1, lng1, lat2, lng2) {
-    const dLon = (lng2 - lng1);
-    const y = Math.sin(dLon) * Math.cos(lat2);
-    const x = (Math.cos(lat1) * Math.sin(lat2)) - (
-      Math.sin(lat1) * Math.cos(lat2) * Math.cos(dLon)
-    );
-    const brng = toDegree(Math.atan2(y, x));
-    return Math.round(360 - ((brng + 360) % 360));
-  }
-
   $('.position').change(() => $('#notices').html('Using a manually entered position.'));
 
   $('#submitposition').click(() => {
@@ -74,17 +35,15 @@ $(document).ready(() => {
     $.each(peaks, (index, element) => {
       const latitude = element.Latitude;
       const longitude = element.Longitude;
+      
+      const homePoint = new LatLon(homeLatitude, homeLongitude);
+      const awayPoint = new LatLon(latitude, longitude);
 
-      const a = { latitude: homeLatitude, longitude: homeLongitude };
-      const b = { latitude, longitude };
-
-      const distance = haversineDistance(a, b);
+      const distance = homePoint.distanceTo(awayPoint);
       const prominence = element.Height / distance;
 
-      let compassBearing;
-
-      if (distance < 100 && distance > 0) {
-        compassBearing = bearing(homeLatitude, homeLongitude, latitude, longitude);
+      if (distance < 10000 && distance > 0) {
+        const compassBearing = homePoint.bearingTo(awayPoint);
 
         orderedPeaks.push({
           Name: element['Hill Name'],
