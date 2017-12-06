f0e0ad34ad31b5e805a7b7ca069740ff1026ba9d
Edward Crompton <edward.crompton@gmail.com>
2017-12-06 21:25:58 +0000

    Updating the gulp file to use the default task correctly

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 // require(d) gulp for compatibility with sublime-gulp.
 var gulp = require('gulp');
 
-gulp.task('default', function(callback) {
+gulp.task('generate-service-worker', function(callback) {
   var path = require('path');
   var swPrecache = require('sw-precache');
   var rootDir = 'web';
 
-  swPrecache.write(path.join(rootDir, 'sw.js'), {
+  swPrecache.write(path.join(rootDir, 'service-worker.js'), {
     staticFileGlobs: [rootDir + '/**/*.{js,html,css,png,jpg,gif}'],
     stripPrefix: rootDir
   }, callback);
 });
+
+gulp.task('default', ['generate-service-worker']);
