5b7816a40fff967d680603e7e8262f7ffb159fde
Edward Crompton <edward.crompton@oxfam.org>
2019-01-18 16:29:30 +0000

    Adding a bash script for deploying to Heroku

---
+++ deploy-to-heroku.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Helper script for deploying latest to Heroku.
+# This assumes at the moment that the Heroku CLI is installed locally. In
+# future we could make this part of the docker stack so that it doesn't
+# have to be installed separately.
+
+# The name of the app on Heroku
+APP_NAME='ebwc-peaks'
+
+heroku container:push web --app "$APP_NAME"
+heroku container:release web --app "$APP_NAME"
+heroku open --app "$APP_NAME"
+
